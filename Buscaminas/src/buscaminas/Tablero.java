02d2ad2abccda2f7564f617ba88f40ac68fdd2e1
medardo <mbonillat@correo.udistrital.edu.co>
2018-03-05 17:51:34 -0500

    Clase buscaCorazones

---
+++ Buscaminas/src/buscaminas/Tablero.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package buscaminas;
+
+/**
+ *
+ * @author DELL
+ */
+public class Tablero {
+  
+   private Celda misCeldas[][];
+   private  Cronómetro miCronómetro;
+   private  Contador miContador;
+   private Emoji miEmoji;
+   public Tablero(){
+        private Sembrador miSembrador=new Sembrador();
+   miSembrador.SembradorCorazones misCeldas,50);
+       int Fila=0;
+       int Columna;
+       for(Fila=0; Fila<10;Fila++){
+           for (Columna<10;Columna++){
+               misCeldas[Fila][Columna]=new Celda();
+               
+           }
+           
+           
+       }
+   }
+    
+    
+    
+    
+    
+}
